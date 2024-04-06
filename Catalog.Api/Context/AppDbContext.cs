e9566956254eec3fb90c35303c9c990de26a9393
Lucas Costa <lucas.co090869@gmail.com>
2024-04-06 14:34:27 -0300

    setting db connection

---
+++ Catalog.Api/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.Api.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Api.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
+}
