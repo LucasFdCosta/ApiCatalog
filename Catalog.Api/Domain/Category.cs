fdf972e2840423fd5bb19fa76a553c28ef386cdf
Lucas Costa <lucas.co090869@gmail.com>
2024-04-06 13:39:15 -0300

    starting asp.net core web api project

---
+++ Catalog.Api/Domain/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalog.Api.Domain;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+}
---
+++ Catalog.Api/Domain/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Catalog.Api.Domain;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
+    public float Stock { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
