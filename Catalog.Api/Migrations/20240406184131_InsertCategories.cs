22b725eb5efdc31585fa099ca8d8deb0f711b40a
Lucas Costa <lucas.co090869@gmail.com>
2024-04-06 16:16:30 -0300

    create migrations to insert data in database

---
+++ Catalog.Api/Migrations/20240406184131_InsertCategories.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categories(Name, ImageUrl) values ('Drinks', 'drinks.jpg')");
+            migrationBuilder.Sql("INSERT INTO Categories(Name, ImageUrl) values ('Food', 'food.jpg')");
+            migrationBuilder.Sql("INSERT INTO Categories(Name, ImageUrl) values ('Dessert', 'dessert.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categories");
+        }
+    }
+}
---
+++ Catalog.Api/Migrations/20240406190700_InsertProducts.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, ImageUrl, Stock, CreatedAt, CategoryId)" +
+                " values ('Diet Coke', 'Diet Coke 350ml', 5.45, 'coke.jpg', 50, now(), 1)");
+            
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, ImageUrl, Stock, CreatedAt, CategoryId)" +
+                " values ('Fries', 'Medium Fries', 4.00, 'fries.jpg', 10, now(), 2)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, ImageUrl, Stock, CreatedAt, CategoryId)" +
+                " values ('Ice Cream', 'Chocolate Ice Cream', 3.00, 'ice-cream.jpg', 15, now(), 3)");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
