c62b14899e36183759c4d8dcd5fa24b1a2334680
Lucas Costa <lucas.co090869@gmail.com>
2024-04-08 20:21:29 -0300

    new: create products controller with 4 endpoints

---
+++ Catalog.Api/Controllers/ProductsController.cs
@@ -0,0 +1,73 @@
+﻿using Catalog.Api.Context;
+using Catalog.Api.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ProductsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var products = _context.Products.ToList();
+
+        if (products is null) return NotFound("No products found!");
+
+        return Ok(products.ToList());
+    }
+
+    [HttpGet("{id:int}", Name="GetProduct")]
+    public IActionResult GetById(int id)
+    {
+        var product = _context.Products.Find(id);
+
+        if (product is null) return NotFound("Product not found!");
+
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public IActionResult Post(Product product)
+    {
+        if (product is null) return BadRequest();
+
+        _context.Products.Add(product);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("GetProduct", new { id = product.Id }, product);
+    }
+
+    [HttpPut("{id:int}")]
+    public IActionResult Put(int id, Product product)
+    {
+        if (id != product.Id) return BadRequest("Invalid ID");
+
+        _context.Entry(product).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(product);
+    }
+
+    [HttpDelete("{id:int}")]
+    public IActionResult Delete(int id)
+    {
+        var product = _context.Products.Find(id);
+
+        if (product is null) return NotFound("Product not found!");
+
+        _context.Products.Remove(product);
+        _context.SaveChanges();
+
+        return Ok($"Deleted product {id}!");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# ApiCatalog
-A simple ASP.NET Core Web Api about products with categories
+# Catalog
+A simple ASP.NET Core Web Api about products with categories built in .NET 8
+
+## Necessary commands (commands that I used while developing)
+
+### in terminal
+```
+dotnet tool install --global dotnet-ef
+```
+This command was necessary to use Entity Framework Core resources.
+
+### in terminal
+```
+dotnet ef migrations add FirstMigration
+```
+This command was necessary to create the migrations
+
+### in terminal
+```
+dotnet ef database update
+```
+This command was necessary to run the migrations
