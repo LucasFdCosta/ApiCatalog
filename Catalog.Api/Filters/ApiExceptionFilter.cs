5a3324905b02c61749d73c54d229ccfc6c26cf77
Lucas Costa <lucas.co090869@gmail.com>
2024-06-10 18:57:06 -0300

    feat: add exception filter to handle exceptions

---
+++ Catalog.Api/Filters/ApiExceptionFilter.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Catalog.Api.Filters
+{
+    public class ApiExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<ApiExceptionFilter> _logger;
+
+        public ApiExceptionFilter(ILogger<ApiExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogError(context.Exception, "An unhandled exception occurred: Status code 500");
+
+            context.Result = new ObjectResult("A problem occurred while handling your request: Status code 500")
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+        }
+    }
+}
--- Catalog.Api/Program.cs
+++ Catalog.Api/Program.cs
@@ -1,4 +1,8 @@
 using Catalog.Api.Context;
+using Catalog.Api.Extensions;
+using Catalog.Api.Filters;
+using Catalog.Api.Logging;
+using Catalog.Api.Services;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 
@@ -6,8 +10,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddJsonOptions(options => options
-    .JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(typeof(ApiExceptionFilter));
+})
+.AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
