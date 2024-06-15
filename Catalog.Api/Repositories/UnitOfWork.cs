6dd2eb2998b6854e3345ab953adbee9bf9970a3f
Lucas Costa <lucas.co090869@gmail.com>
2024-06-14 22:53:30 -0300

    add: add UnitOfWork class
    add UnitOfWork class to implement IUnitOfWork

---
+++ Catalog.Api/Repositories/UnitOfWork.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.Api.Context;
+
+namespace Catalog.Api.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private IProductRepository? _productRepo;
+        private ICategoryRepository? _categoryRepo;
+        public AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IProductRepository ProductRepository
+        { 
+            get
+            {
+                return _productRepo = _productRepo ?? new ProductRepository(_context);
+            }
+        }
+
+        public ICategoryRepository CategoryRepository
+        { 
+            get
+            {
+                return _categoryRepo = _categoryRepo ?? new CategoryRepository(_context);
+            }
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
