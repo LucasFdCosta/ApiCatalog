f386b9d4df74bd36c47b7bd3dcffe056bc326db2
Lucas Costa <lucas.co090869@gmail.com>
2024-06-20 20:20:05 -0300

    feat: add Login endpoint

--- Catalog.Api/Controllers/AuthController.cs
+++ Catalog.Api/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using Catalog.Api.Domain;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Catalog.Api.Domain;
+using Catalog.Api.DTOs;
 using Catalog.Api.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -23,5 +26,48 @@ public class AuthController : ControllerBase
         _userManager = userManager;
         _roleManager = roleManager;
         _configuration = configuration;
+    }
+
+    public async Task<IActionResult> Login([FromBody] LoginModelDTO model)
+    {
+        var user = await _userManager.FindByNameAsync(model.Username);
+
+        if (user is not null && await _userManager.CheckPasswordAsync(user, model.Password!))
+        {
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName!),
+                new Claim(ClaimTypes.Email, user.Email!),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+    
+            foreach (var userRole in userRoles)
+            {
+                authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+            }
+
+            var token = _tokenService.GenerateAccessToken(authClaims, _configuration);
+
+            var refreshToken = _tokenService.GenerateRefreshToken();
+
+            _ = int.TryParse(_configuration["JWT:RefreshTokenValidityInMinutes"], out int refreshTokenValidityInMinutes);
+
+            user.RefreshToken = refreshToken;
+
+            user.RefreshTokenExpiryTime = DateTime.Now.AddMinutes(refreshTokenValidityInMinutes);
+
+            await _userManager.UpdateAsync(user);
+
+            return Ok(new
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                RefreshToken = refreshToken,
+                Expiration = token.ValidTo
+            });
+        }
+
+        return Unauthorized();
     }
 }
