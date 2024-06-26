448744fb60f7c8b5d6f66165ca615290f26f1a83
Lucas Costa <lucas.co090869@gmail.com>
2024-06-26 19:34:03 -0300

    test: add and set configuration for unit tests
    add xUnit test project, install FluentAssertions lib to improve tests and set initial configuration for unit tests

---
+++ Catalog.UnitTests/UnitTests/ProductsUnitTestController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Catalog.Api.Context;
+using Catalog.Api.DTOs.Mappings;
+using Catalog.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.UnitTests.UnitTests
+{
+    public class ProductsUnitTestController
+    {
+        private IUnitOfWork repository;
+        private IMapper mapper;
+        public static DbContextOptions<AppDbContext> dbContextOptions { get; }
+
+        public static string connectionString = "Server=localhost;DataBase=CatalogDB;Uid=root;Pwd=admin";
+
+        static ProductsUnitTestController()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+                .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+                .Options;
+        }
+
+        public ProductsUnitTestController()
+        {
+            var config = new MapperConfiguration(config =>
+            {
+                config.AddProfile(new ProductDTOMappingProfile());
+            });
+
+            mapper = config.CreateMapper();
+            var context = new AppDbContext(dbContextOptions);
+            repository = new UnitOfWork(context);
+        }
+    }
+}
